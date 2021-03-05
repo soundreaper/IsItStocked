f1e83597764c91b3896369b1f3f0cb124089c078
soundreaper@gmail.com <soundreaper@gmail.com>
2021-03-05 08:24:29 +0545

    add functionality to extract key parts of page

--- amazon_scrape.py
+++ amazon_scrape.py
@@ -19,3 +19,30 @@ page = requests.get(prod_tracker_URLS[0], headers=HEADERS)
 
 # Create a `soup` object that makes processing the HTML much easier
 soup = BeautifulSoup(page.content, features="lxml")
+
+# Get product title
+title = soup.find(id='productTitle').get_text().strip()
+
+# Get price of product but if there is no price then this will prevent crashing
+try:
+    price = float(soup.find(id='priceblock_saleprice').get_text().replace('$', '').replace(',', '').strip())
+except:
+    price = ''
+
+# Get review score out of 5 of the product
+review_score = float(soup.select('.a-star-4-5')[0].get_text().split(' ')[0].replace(",", "."))
+
+# Get how many reviews the product has
+review_count = int(soup.select('#acrCustomerReviewText')[0].get_text().split(' ')[0].replace(",", ""))
+
+# Getting the stock state and determining whether the product is in stock
+try:
+    soup.select('#availability .a-color-state')[0].get_text().strip()
+    stock = 'Out of Stock'
+except:
+    stock = 'Available'
+
+print(title)
+print(price)
+print(str(review_score) + ' out of ' + str(review_count) + ' reviews')
+print(stock)
